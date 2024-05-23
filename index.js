4a6dc50a7b8f82357a4ffb6894a2dc4d28ebcedd
lorena1103 <162385884+lorena1103@users.noreply.github.com>
2024-05-22 20:10:11 -0500

    actualizar

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+const express =require("express");
+
+const servidor = express ();
+
+servidor.get("/", (solicitud, respuesta) =>{
+    respuesta.json ({
+        saludo: "hola"
+    } )
+})
+
+
+servidor.listen (3000);
